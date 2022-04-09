0ac46f65b6c9d4b521ad5a8576819e193bde64a0
dhirajkafle553 <dhirajkafle553@gmail.com>
2022-04-09 23:34:18 +0545

    Create Custom Permission Class in permissions.py i.e IsAssigne

--- intern_app/permissions.py
+++ intern_app/permissions.py
@@ -8,11 +8,10 @@ class IsAssignee(BasePermission):
     message = "Unauthorized to perform this action"
 
     def has_permission(self, request, view):
-        print(request.user.is_authenticated)
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return obj.assignee == request.user or obj.assignor.is_supervisor == True
+        return obj.assignee == request.user or request.user.is_supervisor == True
 
