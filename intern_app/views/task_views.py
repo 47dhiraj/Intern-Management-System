6a2c54ce4225e45c2ae9956f3299ff8727875319
dhirajkafle553 <dhirajkafle553@gmail.com>
2022-04-10 15:31:06 +0545

    list() method of ModelViewSet override

--- intern_app/views/task_views.py
+++ intern_app/views/task_views.py
@@ -6,6 +6,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 
+from django.db.models import Q
+
 from intern_app.serializers import TaskSerializer
 
 from ..models import User, Task
@@ -16,6 +18,21 @@ from ..permissions import IsAssignee
 # yo talako TaskViewSet vanni class le ModelViewSet lai extend/inherit gareko cha
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAssignee]
-
+    model = Task
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    lookup_field = 'id'
+
+
+    # Overriding inbuilt methods like list(), retrieve(), create(), update(), partial_update(), destroy()
+
+    def list(self, request):
+        tasks = self.model.objects.filter(Q(assignee=request.user) | Q(assignor=request.user))
+        serializer = self.serializer_class(tasks, many=True)
+        return Response(serializer.data, status=200)
+
+
+
+
+
+
